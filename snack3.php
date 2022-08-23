8286967d8a3100cfbe8cd0fed356210dfa72f946
MatteoGenovese <matteo.genovese@icloud.com>
2022-08-23 12:28:50 +0200

    try snack 3

---
+++ snack3.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<!-- Creare un array con 15 numeri casuali, tenendo conto che l'array non dovrà contenere lo stesso numero più di una volta  -->
+
+    <?php 
+    
+    $arrayRandomNumbers= [];
+
+do{
+
+
+    array_push($arrayRandomNumbers, random_int(0, 15));
+    array_unique($arrayRandomNumbers);
+
+}
+while(count($arrayRandomNumbers)>14);
+
+for ($i=0; $i < count($arrayRandomNumbers);$i++){
+    echo $arrayRandomNumbers[$i];
+}
+
+    
+
+    ?>
+</body>
+</html>
