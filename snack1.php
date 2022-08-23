6349d9dae3adbf23d55dabe1a27b3ef5aa72e3bf
MatteoGenovese <matteo.genovese@icloud.com>
2022-08-23 12:15:38 +0200

    submit snack 2

--- snack1.php
+++ snack1.php
@@ -7,9 +7,12 @@
     <title>Document</title>
 </head>
 <body>
+
+<!-- Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+    Olimpia Milano - Cantù | 55-60 -->
+
     <?php 
-    
-    $tappa1 = [
+        $tappa1 = [
         [
             'teamHome' => 'Milan',
             'teamAway' => 'Inter',
